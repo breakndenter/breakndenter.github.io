8474e20ea8a0c74901b6b517f437513d940d584a
root <kiril_stefanov@outlook.com>
2020-05-21 12:59:51 +0300

    new copy func 8

--- js/copy.js
+++ js/copy.js
@@ -1,59 +1,35 @@
-<script>
-(function(){
+var popupTimer;
 
-	// Get the elements.
-	// - the 'pre' element.
-	// - the 'div' with the 'paste-content' id.
-	
-	var pre = document.getElementsByTagName('pre');
-	var pasteContent = document.getElementById('paste-content');
+function delayPopup(popup) {
+    popupTimer = setTimeout(function() { $(popup).popup('hide') }, 4200);
+}
 
-	// Add a copy button in the 'pre' element.
-	// which only has the className of 'language-'.
-	
-	for (var i = 0; i < pre.length; i++) {
-		var isLanguage = pre[i].children[0].className.indexOf('language-');
+$(document).ready(function () {
+    $('.copyToken').click(function (){
+        clearTimeout(popupTimer);
 
-		if ( isLanguage === 0 ) {
-			var button           = document.createElement('button');
-					button.className = 'copy-button';
-					button.textContent = 'Copy';
+        var $input = $(this).closest('div').find('.copyInput');
 
-					pre[i].appendChild(button);
-		}
-	};
-	
-	// Run Clipboard
-	
-	var copyCode = new Clipboard('.copy-button', {
-		target: function(trigger) {
-			return trigger.previousElementSibling;
-    }
-	});
+        /* Select the text field */
+        $input.select();
 
-	// On success:
-	// - Change the "Copy" text to "Copied".
-	// - Swap it to "Copy" in 2s.
-	// - Lead user to the "contenteditable" area with Velocity scroll.
-	
-	copyCode.on('success', function(event) {
-		event.clearSelection();
-		event.trigger.textContent = 'Copied';
-		window.setTimeout(function() {
-			event.trigger.textContent = 'Copy';
-		}, 2000);
-	});
+        /* Copy the text inside the text field */
+        document.execCommand("copy");
 
-	// On error (Safari):
-	// - Change the  "Press Ctrl+C to copy"
-	// - Swap it to "Copy" in 2s.
-	
-	copyCode.on('error', function(event) { 
-		event.trigger.textContent = 'Press "Ctrl + C" to copy';
-		window.setTimeout(function() {
-			event.trigger.textContent = 'Copy';
-		}, 5000);
-	});
+        $(this)
+            .popup({
+                title    : 'Successfully copied to clipboard!',
+                content  : 'Share this link with your receiver or distributor so they can confirm their order.',
+                on: 'manual',
+                exclusive: true
+            })
+            .popup('show')
+        ;
 
-})();
-</script>
+        // Hide popup after 5 seconds
+        delayPopup(this);
+
+
+    });
+
+});
