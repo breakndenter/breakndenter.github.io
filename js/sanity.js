7dd6fa8f5cd710764428856c68909195649bd7bb
root <kiril_stefanov@outlook.com>
2020-05-22 22:29:56 +0300

    Fully working shells section

--- js/sanity.js
+++ js/sanity.js
@@ -1,4 +1,26 @@
-function myFunction() {
+ip_addr = document.getElementById("IP").value
+port_n = document.getElementById("PORT").value
+res = ip_addr
+res2= port_n
+
+function button(){
+	ip_addr = document.getElementById("IP").value
+	port_n = document.getElementById("PORT").value
+	if ((res === "") || (res2 === "")) {
+		myFunction();
+	}
+	else if ((res != ip_addr) || (res2 != port_n)) {
+	reset();
+	myFunction();
+	}
+	else {
+		reset();
+	}
+}
+
+
+
+function myFunction(){
         var patt = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
         var patp = /^()([1-9]|[1-5]?[0-9]{2,4}|6[1-4][0-9]{3}|65[1-4][0-9]{2}|655[1-2][0-9]|6553[1-5])$/;
         var ip_addr = document.getElementById("IP").value
@@ -7,7 +29,7 @@ function myFunction() {
 if(ip_addr == ' ')
         {
         txt = "IP Address field can not be empty";
-        document.getElementById("result").innerHTML = txt;
+        document.getElementById("result").innerHTML = txt
         return false;
         }
     else if (port_n == '')  
@@ -26,18 +48,8 @@ if(ip_addr == ' ')
      {  
         txt = "Port must be between 0-65535";
         document.getElementById("result").innerHTML = txt;
-        return false;  
+        return false; 
      }
-    else if (ip_addr_changed.equals(ip_addr) == false)
-	{
-		function reset();
-		continue;
-	}
-    else if (port_nu_changed.equals(port_n) == false)
-	{
-		function reset();
-		continue;
-	}
     else {
 document.getElementById("btClear").disabled = false;
 var elements = document.getElementsByClassName('highlight');
@@ -56,29 +68,31 @@ for (var p = 0; p < elements.length; p++) {
   elements[p].innerHTML = port;
   txt = "You are now ready to copy your desired shell down below";
   document.getElementById("result").innerHTML = txt;
-  var ip_addr_changed = document.getElementById("IP").value;
-  var port_nu_changed = document.getElementById("PORT").value;
+  var ip_addr = document.getElementById("IP").value;
+  var port_n = document.getElementById("PORT").value;
+  res = ip_addr
+  res2 = port_n
+  document.getElementById("IP").value = "";
+  document.getElementById("PORT").value = "";
   return true;
 }
 }
 }
 
 
-function reset() {
+function reset(){
   var elements = document.getElementsByClassName('highlight');
   for (var i = 0; i < elements.length; i++) {
   var str = elements[i].innerHTML;
-  var r_ip = document.getElementById("IP").value;
-  var ip = str.replace(r_ip, "YOUR_IP");
+  var ip = str.replace(res, "YOUR_IP");
   elements[i].innerHTML = ip;
 }
   for (var p = 0; p < elements.length; p++) {
   var str = elements[p].innerHTML;
-  var r_port = document.getElementById("PORT").value;
-  var port = str.replace(r_port, "PORT");
+  var port = str.replace(res2, "PORT");
   elements[p].innerHTML = port;
 }
   document.getElementById("btClear").disabled = true;
-  txt = ""
+  txt = "reset was done here"
   document.getElementById("result").innerHTML = txt;
 }
